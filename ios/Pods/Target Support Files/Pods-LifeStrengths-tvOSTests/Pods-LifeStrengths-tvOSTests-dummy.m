e1a2efd4e6935c7968f196f3764c6f07c5bfa388
Sean Templeton <sean@templeton.io>
2017-11-04 15:57:41 -0500

    Fix broken project

---
+++ ios/Pods/Target Support Files/Pods-LifeStrengths-tvOSTests/Pods-LifeStrengths-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LifeStrengths_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_LifeStrengths_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-LifeStrengthsTests/Pods-LifeStrengthsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LifeStrengthsTests : NSObject
+@end
+@implementation PodsDummy_Pods_LifeStrengthsTests
+@end
