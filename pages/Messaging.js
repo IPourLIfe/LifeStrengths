767bda66f993e4d242f362052508ff6bbc79a93f
Sean Templeton <sean@templeton.io>
2017-11-05 12:19:44 -0600

    Add finishing UI touches

--- pages/Messaging.js
+++ pages/Messaging.js
@@ -8,11 +8,11 @@ export default Messaging = () => (
         <View style={{height: 60, width: '100%', backgroundColor: '#00BCD4', paddingTop: 20, paddingLeft: 20}}>
             <Text style={{color: '#FFF', fontSize: 20, fontWeight: 'bold'}}>Messages</Text>
         </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-            <View style={{width: 75}}>
+        <View style={{flex: 1, flexDirection: 'row', backgroundColor: '#EEE'}}>
+            <View style={{width: 75, position: 'absolute', zIndex: 10, top: 0, left: 0, bottom: 0}}>
                 <Channels/>
             </View>
-            <View style={{flex: 1}}>
+            <View style={{flex: 1, marginLeft: 75, backgroundColor: '#FFF'}}>
                 <ChatBox/>
             </View>
         </View>
--- pages/messaging/Channels.js
+++ pages/messaging/Channels.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {View, Text, ScrollView} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {MKButton} from 'react-native-material-kit';
 
 const channelButtonStyle = {
     backgroundColor: '#FFF',
@@ -16,7 +17,7 @@ const channelButtonStyle = {
 const channelButtonContentStyle = {
     width: '100%',
     backgroundColor: 'transparent',
-    textAlign: 'center'
+    textAlign: 'center',
 };
 
 const activeChannel = {
@@ -24,28 +25,45 @@ const activeChannel = {
     width: 75,
     height: 75,
     margin: 0,
-    borderRadius: 0
+    borderRadius: 0,
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    shadowOffset: {
+        width: -1,
+        height: 1
+    }
 };
+
+const ColoredFab = MKButton.coloredFab()
+    .withStyle({
+        margin: 10,
+        width: 55,
+        height: 55
+    });
+
+const AddConvoButton = ColoredFab.withBackgroundColor('#FFB100').build();
+const GeneralButton = ColoredFab.withBackgroundColor('#FFF').build();
 
 
 export default Channels = () => (
-    <View style={{flex: 1, backgroundColor: '#EEE'}}>
+    <View style={{flex: 1}}>
         <ScrollView style={{flex: 1}}>
-            <View style={channelButtonStyle}>
+            <GeneralButton>
                 <MaterialIcons name='people-outline' size={26} style={channelButtonContentStyle}/>
-            </View>
+            </GeneralButton>
             <View style={activeChannel}>
                 <Text style={channelButtonContentStyle}>LC</Text>
             </View>
-            <View style={channelButtonStyle}>
+            <GeneralButton>
                 <Text style={channelButtonContentStyle}>CC</Text>
-            </View>
-            <View style={{...channelButtonStyle, backgroundColor: '#FFB100'}}>
-                <MaterialIcons name='add' size={26} style={channelButtonContentStyle}/>
-            </View>
+            </GeneralButton>
+            <AddConvoButton>
+                <MaterialIcons name='add' size={26} style={{...channelButtonContentStyle, 'color': '#FFF'}}/>
+            </AddConvoButton>
         </ScrollView>
-        <View style={channelButtonStyle}>
+        <GeneralButton>
             <MaterialCommunityIcons name='alert-decagram' size={26} style={{...channelButtonContentStyle, color: 'red'}} />
-        </View>
+        </GeneralButton>
     </View>
 );
