0817d42722e76e8a18205abbcd5348e68234961d
Sean Templeton <sean@templeton.io>
2017-11-05 01:38:43 -0500

    Add mocks for lesson view

---
+++ pages/curriculum/LifeMap.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default LifeMap = () => (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Curriculum</Text>
+    </View>
+);
